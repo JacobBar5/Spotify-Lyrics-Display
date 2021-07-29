d52dab1b838082f0b0ebae3e0e6248c856f6a1c5
Jacob <jbarberena@miners.utep.edu>
2021-07-29 16:54:12 -0600

    Updated and cleaned objectives list

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 #region
+#
 # Objectives needed to get lyrics:
-#  - detect song that playing on spotify
-#    To accomplish:
-#     - look into a spotify API or similar
-#     - get artist and song name
-#  - crawl website of song and get the lyrics
-#    To accomplish:
-#     - need to detect song spotify is playing
-#     - place playing song into azlyrics.com search
+#  - detect song that playing on spotify ----------> Spotify API used
+#  - get artist and song name
+#  - crawl website of song and get the lyrics ----------> Genius API used instead
 #        NOTE: azlyrics.com
 #        <!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->
-#                > look into Genius API
-#                   >>> pip install lyricsgenius
-#  - display lytrics in app window
-#    To accomplish:
-#     - need create app window
-#     - allow text on window to be able to be updated
-#     - (?) try to predict/get next song that will played ahead of time
+#     
+# Objectives to display lyrics:
+#  - create app window WITH updating on song change funtionality
+#     - need song change check
+#  - try to predict/get next song that will played ahead of time (?)
+#
 #endregion
 
 from apis.getLyrics import Lyrics
